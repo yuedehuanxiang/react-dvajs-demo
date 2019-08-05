3dbe6d73a62fb8b237ab1971c062c510735074c4
chenshuai <shuai.chen@wiz.ai>
2019-08-05 11:48:13 +0800

    安装antd sass 和配置 webpackrc 来按需加载

---
+++ .webpackrc.js
@@ -0,0 +1,16 @@
+// const path = require("path");
+
+// export default {
+//   extraBabelPlugins: [
+//     ["import", { libraryName: "antd", libraryDirectory: "es", style: true }]
+//   ],
+//   alias: {
+//     Assets: path.resolve(__dirname, "./src/assets")
+//   }
+// };
+
+{
+  extraBabelPlugins: [
+    ["import", { libraryName: "antd", libraryDirectory: "es", style: true }]
+  ];
+}
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { connect } from "dva";
-// import styles from "./IndexPage.css";
+import styles from "./IndexPage.scss";
+import { Layout } from "antd";
+
+const { Header, Content } = Layout;
 
 function IndexPage() {
-  return <div>hello world</div>;
+  return (
+    <Layout className={styles.layout}>
+      <Header className={styles.header}>header</Header>
+
+      <Content className={styles.content}>content</Content>
+    </Layout>
+  );
 }
 
 IndexPage.propTypes = {};
